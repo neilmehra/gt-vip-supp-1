18162497a9f014afe1bc7fe256f51fca2b01afd5
neil mehra <neilmehra@outlook.com>
2025-02-04 23:38:16 -0500

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 awk 'BEGIN {FS="\t"} /^#CHROM/ {print; next} $0 !~ /^#/ && $7=="PASS"' filtered_snps_FR07_t100_N1_lane1.vcf > passed_variants.vcf
-awk 'BEGIN {FS="\t"} !/^#/ { split($10, a, ":"); print a[2] ":" a[3] }' passed_variants.vcf > ok.vcf
+awk 'BEGIN {FS="\t"} !/^#/ { split($10, a, ":"); print a[2] ":" a[3] }' passed_variants.vcf > d3.vcf
 
 awk 'BEGIN {FS="\t"} 
      !/^#/ {
